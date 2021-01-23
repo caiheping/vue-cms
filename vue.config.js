8e061017dcc3905999415802025a3227d656ec1a
chp <1670341607@qq.com>
2021-01-23 16:26:45 +0800

    修改前端静态文件路径

--- vue.config.js
+++ vue.config.js
@@ -5,7 +5,7 @@ function resolve (dir) {
 }
 
 module.exports = {
-  publicPath: process.env.NODE_ENV === 'production' ? '/admin' : '/',
+  publicPath: process.env.NODE_ENV === 'production' ? '/' : '/',
   lintOnSave: true,
   // 打包时不生成.map文件
   productionSourceMap: false,
